1361a6779a9dcf0256d022937ba682b36c78c43e
mavor <mauricio.pasten.martinez@gmail.com>
2024-10-20 01:22:43 -0600

    using slf4j instead of sout

--- src/main/java/com/mavor/HelloWorld.java
+++ src/main/java/com/mavor/HelloWorld.java
@@ -1,11 +1,14 @@
 package com.mavor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  * Hello world!
  *
  */
 public class HelloWorld {
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
     public void sayHello() {
-        System.out.println("Hello World!");
+        logger.info("Hello World!");
     }
 }
