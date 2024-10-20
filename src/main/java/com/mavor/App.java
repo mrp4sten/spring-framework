35c616387ca3293c555f5bb56ff1bbad408669c4
mavor <mauricio.pasten.martinez@gmail.com>
2024-10-20 17:04:37 -0600

    adding app sample

--- src/main/java/com/mavor/App.java
+++ src/main/java/com/mavor/App.java
@@ -1,16 +1,28 @@
 package com.mavor;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.sql.Date;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.mavor.config.AppConfig;
+import com.mavor.models.Todo;
+import com.mavor.service.TodoService;
 
 public class App {
   public static void main(String[] args) {
-    // Load Spring context from the XML file
-    ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-    // Get the HelloWorld bean and call the sayHello method
-    HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
-    helloWorld.sayHello();
+    try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class)) {
+        TodoService todoService = context.getBean(TodoService.class);
+
+        Todo newTodo = new Todo("this is a title", "description for my task", new Date(System.currentTimeMillis()), false);
+        todoService.save(newTodo);
+
+        Todo newTodo2 = new Todo("this is a title 2", "description for my task 2", new Date(System.currentTimeMillis()), false);
+        todoService.save(newTodo2);
+
+        todoService.findAll()
+            .forEach(todo -> System.out.println("Todo: " + todo.getTitle() + ", Completed: " + todo.isDone()));
+    }
   }
 
 }
